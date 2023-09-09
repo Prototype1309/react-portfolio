17366d9375dfbf1ffe961c55c59f2394323f4a8e
prototype1309 <simo.hourri@gmail.com>
2023-09-09 14:51:20 -0400

    change cassing

--- src/components/portfolioPage.jsx
+++ src/components/portfolioPage.jsx
@@ -1,15 +1,17 @@
 
 import React from "react";
-import portfolio from "../data/portfolio";
+import portfolioData from "../data/Portfolio";
 import PortfolioItem from "./PortfolioItem";
+import { v4 as uuidv4 } from "uuid";
 
 function Portfolio() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {portfolio.map((project) => (
-          // eslint-disable-next-line react/jsx-key
-          <PortfolioItem
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" >
+        
+        {portfolioData.map((project) => (
+          <PortfolioItem 
+            key={uuidv4}
             imgUrl={project.imgUrl}
             title={project.title}
             stack={project.stack}
