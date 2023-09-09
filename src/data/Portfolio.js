357e8e1139e3d8232433ec4222ecb43514a8bd84
prototype1309 <simo.hourri@gmail.com>
2023-09-09 14:58:39 -0400

    adding IMG ..

--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -1,37 +1,37 @@
 export default [
   {
     title: "Zooquarium",
-    imgUrl: "src/assets/Project1SS.PNG",
+    imgUrl: "../src/assets/Project1SS.PNG",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://caitlinramsey.github.io/zooquarium/",
   },
   {
     title: "BugBytes",
-    imgUrl: "src/assets/main.JPG",
+    imgUrl: "../src/assets/main.JPG",
     stack: ["React", "Tailwind", "Vite"],
     link: "https://bugbytes-eea33095845f.herokuapp.com/",
   },
   {
     title: "Discover Devs",
-    imgUrl: "/src/assets/discoverDevs.png",
+    imgUrl: "../src/assets/discoverDevs.png",
     stack: ["Handlebars", "CSS", "JavaScript", "NoSQL"],
     link: "https://discoverdevs-2aa18af2a0dc.herokuapp.com/home",
   },
   {
     title: "City Weather App",
-    imgUrl: "src/assets/Weather-app.png",
+    imgUrl: "../src/assets/Weather-app.png",
     stack: ["HTML", "CSS", "JavaScript", "Api's"],
     link: "https://prototype1309.github.io/City-Weather-App/",
   },
   {
     title: "Text Editor",
-    imgUrl: "src/assets/Jate.png",
+    imgUrl: "../src/assets/Jate.png",
     stack: ["concurrently", "nodemon", "webpack", "JavaScript"],
     link: "https://textteditorr-758cd3961530.herokuapp.com/",
   },
   {
     title: "Workday Scheduler",
-    imgUrl: "src/assets/Workday-Scheduler.gif",
+    imgUrl: "../src/assets/Workday-Scheduler.gif",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://prototype1309.github.io/workday-scheduler-/",
   },
